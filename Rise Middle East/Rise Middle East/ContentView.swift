3b39fb9f4e55d4916ffe4a3eb113e096b952a0c4
Alankrita <alankritabiswas568@gmail.com>
2026-06-19 15:02:28 +0300

    completely fixed nested git folder

---
+++ Rise Middle East/Rise Middle East/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rise Middle East
+//
+//  Created by Alankrita Biswas on 6/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rise Middle East/Rise Middle East/Rise_Middle_EastApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rise_Middle_EastApp.swift
+//  Rise Middle East
+//
+//  Created by Alankrita Biswas on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Rise_Middle_EastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
